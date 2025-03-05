fab5dadd1f47bdbeb4f9a85693c01cda0c1dbeca
Marc Stirner <marc.stirner@hotmail.de>
2025-03-05 13:10:26 +0100

    updated

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Introduction
-This is a boilerplate plugin.
+Integrates remote controlling of devices via API.<br />
+https://www.fully-kiosk.com/en/#rest
+
+> [!IMPORTANT]  
+> This plugin uses [Fully Kiosk](https://github.com/3cky/mbusd) Plus features.<br />
+
+> [!NOTE]
+> Change the IP Adress from `127.5.5.1` to your devices real IP
+> Set the API password secret in vaults
+
 
 # Installation
 1) Create a new plugin over the OpenHaus backend HTTP API
@@ -11,23 +20,17 @@ Add plugin item via HTTP API:<br />
 [PUT] `http://{{HOST}}:{{PORT}}/api/plugins/`
 ```json
 {
-   "name":"Plugin Boilerplate",
-   "version":1,
+   "name":"Fully Kiosk Integration",
+   "version": "1.0.0",
    "intents":[
       "devices",
-      "endpoints",
-      "plugins",
-      "rooms",
-      "ssdp",
-      "store",
-      "users",
-      "vault"
+      "endpoints"
    ],
-   "uuid": "00000000-0000-0000-0000-000000000000"
+   "uuid": "e973c5e3-9188-4381-8c04-59aa9c7e2f48"
 }
-
 ```
+
 Mount the source code into the backend plugins folder
 ```sh
-sudo mount --bind ~/projects/OpenHaus/plugins/plugin-boilerplate/ ~/projects/OpenHaus/backend/plugins/00000000-0000-0000-0000-000000000000/
+sudo mount --bind ~/projects/OpenHaus/plugins/oh-plg-fully-kiosk/ ~/projects/OpenHaus/backend/plugins/e973c5e3-9188-4381-8c04-59aa9c7e2f48/
 ```
--- index.js
+++ index.js
@@ -1,41 +1,154 @@
+const COMMANDS = require("./commands.json");
+const request = require("../../helper/request.js");
+
+const { URL } = require("url");
+
+const PASSWORD = "Pa$$w0rd";
+
 module.exports = (info, logger, init) => {
     return init([
         "devices",
         "endpoints",
-        "plugins",
-        "rooms",
-        "ssdp",
-        "store",
-        "users",
         "vault"
     ], (scope, [
         C_DEVICES,
         C_ENDPOINTS,
-        C_PLUGINS,
-        C_ROOMS,
-        C_SSDP,
-        C_STORE,
-        C_USERS,
         C_VAULT
     ]) => {
 
 
-        // do something here with the components
-        // documentation about them can be found on:
-        // https://docs.open-haus.io/#/backend/components/
+        C_DEVICES.found({
+            labels: [
+                "app=fully",
+                "type=kiosk"
+            ]
+        }, (device) => {
+
+            C_ENDPOINTS.found({
+                device: device._id,
+                //labels: device.labels
+            }, (endpoint) => {
 
 
-        // scope = plugin class instace
-        console.log(scope);
+                C_VAULT.found({
+                    identifier: endpoint._id
+                }, (vault) => {
 
+                    let iface = device.interfaces[0];
+                    let { host, port } = iface.settings;
+                    let agent = iface.httpAgent();
+                    let secret = vault.secrets[0];
 
-        // example logger usage
-        logger.trace("Hello World from plugin ", info.name);
-        logger.verbose("Hello World from plugin ", info.name);
-        logger.debug("Hello World from plugin ", info.name);
-        logger.info("Hello World from plugin ", info.name);
-        logger.warn("Hello World from plugin ", info.name);
-        logger.error("Hello World from plugin ", info.name);
+                    endpoint.commands.forEach((command) => {
+                        command.setHandler((cmd, iface, params, done) => {
+
+                            let url = new URL(`http://${host}:${port}?cmd=${cmd.alias}&type=json&password=${secret.decrypt()}`);
+
+                            if (cmd.alias === "textToSpeech") {
+
+                                let today = new Date();
+                                let day = today.toLocaleDateString('de-DE', { weekday: 'long' });
+                                let month = today.toLocaleDateString('de-DE', { month: 'long' });
+
+                                let text = "Guten Morgen Marc, ";
+                                text += `Es ist ${day} der ${today.getDate()} ${month}. `;
+                                text += "Zeit zum aufstehen.";
+
+                                url.searchParams.set("text", text);
+
+                            }
+
+                            // handle settings key/value
+                            if (["screenBrightness", "keepScreenOn"].includes(cmd.alias)) {
+
+                                // param type = cmd value
+                                // boolean = setBooleanSetting
+                                // number = setStringSetting
+                                // string = setStringSetting
+
+                                let { type, value } = params[0];
+
+                                switch (type) {
+                                    case "string": url.searchParams.set("cmd", "setStringSetting"); break;
+                                    case "number": url.searchParams.set("cmd", "setStringSetting"); break;
+                                    case "boolean": url.searchParams.set("cmd", "setBooleanSetting"); break;
+                                }
+
+                                url.searchParams.set("key", cmd.alias);
+                                url.searchParams.set("value", value);
+
+                            }
+
+                            console.log("UZRL", url.toString())
+
+                            request(url.toString(), {
+                                agent
+                            }, (err, result) => {
+                                if (err) {
+
+                                    done(err);
+
+                                } else {
+
+                                    done(null, result.body?.status === "OK");
+
+                                }
+                            });
+
+                        });
+                    });
+
+                }, async (filter) => {
+
+                    let vault = await C_VAULT.add({
+                        ...filter,
+                        name: `Fully Kiosk Password (${device._id})`,
+                        secrets: [{
+                            name: "Password",
+                            key: "password"
+                        }]
+                    });
+
+                    logger.info(`Vault created for device ${device._id}`);
+
+                });
+
+            }, async (filter) => {
+
+                let endpoint = await C_ENDPOINTS.add({
+                    name: device.name,
+                    icon: "fa-solid fa-tablet-screen-button",
+                    ...filter,
+                    commands: COMMANDS.map((cmd) => {
+
+                        cmd.interface = device.interfaces[0]._id;
+
+                        return cmd;
+
+                    })
+                });
+
+                logger.info(`Endpoint "${endpoint.name}" added`);
+
+            });
+
+        }, async (filter) => {
+
+            let device = await C_DEVICES.add({
+                name: "Fully Kiosk App",
+                icon: "fa-solid fa-tablet-screen-button",
+                ...filter,
+                interfaces: [{
+                    settings: {
+                        host: "127.5.5.1",
+                        port: 2323
+                    }
+                }]
+            });
+
+            logger.info(`Device "${device.name}" added`);
+
+        });
 
 
     });
